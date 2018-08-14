e8ac46ac510b6e98f7c213fdbefb310b8485d813
Steve Phelps <79sphelps@gmail.com>
2018-08-14 14:38:21 -0700

    Updating server.js

--- server.js
+++ server.js
@@ -51,7 +51,8 @@ app.set("port", port);
 // Set static path to Angular app in dist
 // Don't run in dev
 if (process.env.NODE_ENV !== "dev") {
-  app.use("/", express.static(path.join(__dirname, "/dist")));
+  app.use("/", express.static("/dist"));
+  //app.use("/", express.static(path.join(__dirname, "/dist")));
   //app.use("/", express.static(path.join("/dist/event-planning-nw")));
 }
 
@@ -75,7 +76,8 @@ require("./server/api")(app, config);
 // Don't run in dev
 if (process.env.NODE_ENV !== "dev") {
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/dist/index.html"));
+    res.sendFile("/dist/index.html");
+    //res.sendFile(path.join(__dirname, "/dist/index.html"));
     //res.sendFile(path.join("/dist/event-planning-nw/index.html"));
   });
 }
