6c27d323d366fb1c19eb79f661e00fc015361343
Steve Phelps <79sphelps@gmail.com>
2018-08-15 09:10:57 -0700

    Updating for iOS

--- server/api.js
+++ server/api.js
@@ -4,26 +4,8 @@
  | Dependencies
  |--------------------------------------
  */
-
 const jwt = require("express-jwt");
 const jwks = require("jwks-rsa");
-
-/*
-const Contact = require("./models/Contact");
-const Footer = require("./models/Footer");
-const HairColoring = require("./models/Hair_Coloring");
-const HairExtensionsAndSmoothing = require("./models/Hair_Extensions_and_Smoothing");
-const HaircutAndStyle = require("./models/Haircut_and_Style");
-const Homepage = require("./models/Homepage");
-const Images = require("./models/Images");
-const LashExtensions = require("./models/Lash_Extensions");
-const Personel = require("./models/Personel");
-const Request = require("./models/Request");
-const Services = require("./models/Services");
-const SpecialOccasionsAndTreatments = require("./models/Special_Occasions_and_Treatments");
-const Testimonials = require("./models/Testimonials");
-const Waxing = require("./models/Waxing");
-*/
 
 /*
  |--------------------------------------
@@ -54,7 +36,6 @@ module.exports = function(app, config) {
     }
   };
 
-
   // GET API root
   app.get("/api/", (req, res) => {
     res.send("API Works");
@@ -65,44 +46,5 @@ module.exports = function(app, config) {
   require(appRoot + "/server/routes/homepage.router")(app, jwtCheck, adminCheck);
   require(appRoot + "/server/routes/personel.router")(app, jwtCheck, adminCheck);
   require(appRoot + "/server/routes/services.router")(app, jwtCheck, adminCheck);
-
-  /*
-  require("./routes/contact.router")(app, jwtCheck, adminCheck);
-  require("./routes/gallery.router")(app, jwtCheck, adminCheck);
-  require("./routes/homepage.router")(app, jwtCheck, adminCheck);
-  require("./routes/personel.router")(app, jwtCheck, adminCheck);
-  require("./routes/services.router")(app, jwtCheck, adminCheck);
-  */
   
- 
-  //require("./routes/homepage.router")(app)
-
-
-  //const ctrlHomepage = require("../controllers/homepage.controller");
-  //app.get('/homepage', jwtCheck, adminCheck, ctrlHomepage.getHomepage);
-
-  /*
-  const _homepageProjection = "headline headlineSubMsg";
-  //const _homepageProjection = 'aboutQuote';
-
-  app.get("/api/homepage", function(req, res, next) {
-    Homepage.find({}, (err, homepageDetails) => {
-      let homepageArr = [];
-      if (err) {
-        return res.status(500).send({ message: err.message });
-      }
-
-      console.log(homepageDetails);
-
-      if (homepageDetails) {
-        homepageDetails.forEach(homepageDetail => {
-          homepageArr.push(homepageDetail);
-        });
-      }
-
-      res.send(homepageArr[0]);
-    });
-  });
-  */
-
 };
