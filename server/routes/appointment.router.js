2a49d9f5e68cb7b95ef3d576e122217e87fd51f6
Steve Phelps <79sphelps@gmail.com>
2018-08-17 11:19:55 -0700

    Adding back calendar API

--- server/routes/appointment.router.js
+++ server/routes/appointment.router.js
@@ -3,7 +3,7 @@ const ctrlAppointments = require('../controllers/appointments.controller');
 
 module.exports = function(app, jwtCheck, adminCheck) {
     app.get('/api/admin/appointments', ctrlAppointments.getAppointments);
-    app.get('/api/admin/appointments/:id', jwtCheck, ctrlAppointments.getRequestById);
+    app.get('/api/admin/appointments/:id', jwtCheck, ctrlAppointments.getAppointmentById);
     app.post('/api/appointments/new', ctrlAppointments.create);
     app.put('/api/admin/appointments/update/:id', jwtCheck, adminCheck, ctrlAppointments.update);
     app.delete('/api/admin/appointments/:id', jwtCheck, adminCheck, ctrlAppointments.delete);
